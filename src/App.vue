1d1d9e38191326fb2a513c5e0870afbd52ed8ec7
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-16 21:12:13 -0500

    shop layout - shop design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,3 @@
-import { RouterView } from 'vue-router';
 <template>
-  <h1>Hello World!</h1>
   <RouterView/>
 </template>
---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <TopMenu/>
+  <RouterView/>
+  <CustomFooter/>
+</template>
+<script setup lang="ts">
+  import CustomFooter from '../components/CustomFooter.vue';
+  import TopMenu from '../components/TopMenu.vue';
+</script>
