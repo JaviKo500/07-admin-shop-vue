eab643a831d8494b3179f03c321eeae6fac2153e
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-21 20:54:01 -0500

    add user name and log out

--- src/modules/admin/layouts/AdminLayout.vue
+++ src/modules/admin/layouts/AdminLayout.vue
@@ -3,11 +3,11 @@
 
 	<!-- Component Start -->
     <div class="flex flex-col items-center w-16 pb-4 overflow-auto border-r border-gray-300">
-        <a class="flex items-center justify-center flex-shrink-0 w-full h-16 bg-gray-300" href="#">
+        <RouterLink to="/admin" class="flex items-center justify-center flex-shrink-0 w-full h-16 bg-gray-300">
             <svg class="w-8 h-8"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
             </svg>
-        </a>
+        </RouterLink>
         <a class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300" href="#">
             <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
@@ -90,7 +90,7 @@
     </div>
     <div class="flex flex-col flex-grow">
         <div class="flex items-center flex-shrink-0 h-16 px-8 border-b border-gray-300">
-            <h1 class="text-lg font-medium">Page Title</h1>
+            <h1 class="text-lg font-medium">Hello, {{ authStore.username  }}</h1>
             <button class="flex items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-300">
                 Action 1
             </button>
@@ -104,9 +104,7 @@
                     </svg>
                 </div>
                 <div class="absolute right-0 flex-col items-start hidden w-40 pb-1 bg-white border border-gray-300 shadow-lg group-focus:flex">
-                    <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
-                    <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
-                    <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
+                    <a @click="authStore.logout" class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Logout</a>
                 </div>
             </button>
         </div>
@@ -116,12 +114,18 @@
     </div>
     <!-- Component End  -->
 
-</div>
+    </div>
 
-<a class="fixed flex items-center justify-center h-8 pr-2 pl-1 bg-blue-600 rounded-full bottom-0 right-0 mr-4 mb-4 shadow-lg text-blue-100 hover:bg-blue-600" href="https://twitter.com/lofiui" target="_top">
-	<div class="flex items-center justify-center h-6 w-6 bg-blue-500 rounded-full">
-		<svg viewBox="0 0 24 24" class="w-4 h-4 fill-current"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
-	</div>
-	<span class="text-sm ml-1 leading-none">@lofiui</span>
-</a>
+    <a class="fixed flex items-center justify-center h-8 pr-2 pl-1 bg-blue-600 rounded-full bottom-0 right-0 mr-4 mb-4 shadow-lg text-blue-100 hover:bg-blue-600" href="https://twitter.com/lofiui" target="_top">
+        <div class="flex items-center justify-center h-6 w-6 bg-blue-500 rounded-full">
+            <svg viewBox="0 0 24 24" class="w-4 h-4 fill-current"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+        </div>
+        <span class="text-sm ml-1 leading-none">@lofiui</span>
+    </a>
 </template>
+
+<script setup lang="ts">
+    import { useAuthStore } from '@/modules/auth/stores/auth.store';
+
+    const authStore = useAuthStore();
+</script>
