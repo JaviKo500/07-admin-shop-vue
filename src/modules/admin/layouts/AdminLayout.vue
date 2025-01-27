6926b4e579bb8a445ba5fb3bd8d709b4b77893c8
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-27 05:33:22 -0500

    check min width side bar icons

--- src/modules/admin/layouts/AdminLayout.vue
+++ src/modules/admin/layouts/AdminLayout.vue
@@ -2,7 +2,7 @@
   <div class="flex w-screen h-screen text-gray-700">
 
 	<!-- Component Start -->
-    <div class="flex flex-col items-center w-16 pb-4 overflow-auto border-r border-gray-300">
+    <div class="flex flex-col items-center w-16 pb-4 overflow-auto border-r border-gray-300 min-w-[60px]">
         <RouterLink to="/admin" class="flex items-center justify-center flex-shrink-0 w-full h-16 bg-gray-300">
             <svg class="w-8 h-8"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
