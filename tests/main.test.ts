3d844d20ecbc5bb4351859dd19fa1ff065873ac6
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-30 06:22:36 -0500

    init test and env dev

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,7 @@
 describe('Main.test', () => {
   test( 'test', () => {
+    console.log('<--------------- JK Main.test --------------->');
+    console.log(import.meta.env);
     expect(true).toBe(true);
   });
 });
